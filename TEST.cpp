a5ebc69b10d2ef7a204897cd258f9304bbb8f9c5
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-07-08 11:04:54 +0530

    Update TEST.cpp

--- TEST.cpp
+++ TEST.cpp
@@ -6,7 +6,7 @@
 TEST_CASE("process input sensors value and send to receiver") {
   int sensordatalen = 8;
    float TempArray[sensordatalen] ={2,3, 3, 5, 4, 10, 11, 12};
-   int ChargerateArray[sensordatalen] ={2,3, 3, 5, 4, 10, 11, 12};
+   float ChargerateArray[sensordatalen] ={2,3, 3, 5, 4, 10, 11, 12};
 
    REQUIRE(Main(TempArray,ChargerateArray,sensordatalen,3,10,5,11) == 1);
    
