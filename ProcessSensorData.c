f080dd9b7919c1c9d33b6ef18a8f9e8025ea74ff
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-07-08 08:04:42 +0530

    Update ProcessSensorData.c

--- ProcessSensorData.c
+++ ProcessSensorData.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+
+float chargerate_minvalue = 0.79;
+float chargerate_maxvalue = 0.85;
+float temp_minvalue       = 0.0; /*in Farenheit*/
+float temp_manvalue       = 45.0;/*in Farenheit*/
+
+_Bool CheckChargeRateInRange(float chargeRate)
+  {
+      if(chargeRate < chargerate_minvalue|| temperature > chargerate_maxvalue)
+      {
+          return 0;
+      }
+      else
+      {
+        return 1;  
+      }
+  }
+_Bool CheckTempInRange(float temperature)
+  {
+      if(temperature < temp_minvalue|| temperature > temp_manvalue)
+      {
+          return 0;
+      }
+      else
+      {
+        return 1;  
+      }
+  }
+
+ float GetTempInFarenheit(float temperature,char tempunit)
+  {
+      float temp;
+      if(tempunit == 'C')
+      {
+          temp = ((temperature * 9/5) + 32);
+      }
+      else
+      {
+          temp = temperature;
+      }
+      return temp;
+  }
