0b0e6c85314fef3bef1c6d5d2c7ee4fc5c97c5fc
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-07-14 23:16:27 +0530

    Update TEST.cpp

--- TEST.cpp
+++ TEST.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "Sender.c"
+#include "Sender.h"
 
 TEST_CASE("process input sensors value and send to receiver") {
   
