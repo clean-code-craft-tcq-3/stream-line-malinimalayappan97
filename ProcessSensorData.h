16fbc85561299d82f2cd9f09e10ad8eadbf1d727
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-07-14 23:08:20 +0530

    Delete ProcessSensorData.h

--- ProcessSensorData.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-_Bool CheckChargeRateInRange(float chargeRate);
-_Bool CheckTempInRange(float temperature);
-int PrintOnConsole(float temperature, float chargerate);
-int Main(float* sensor_temprature,float* sensor_chargerate,int sensordatalength,float temp_min,float temp_max,float chargerate_min,float chargerate_max);
