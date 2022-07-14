30af0fabaa35e56a61edf080d0a400e82f5b625a
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-07-14 23:10:24 +0530

    Update TEST.cpp

--- TEST.cpp
+++ TEST.cpp
@@ -1,13 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "ProcessSensorData.h"
+#include "Sender.c"
 
 TEST_CASE("process input sensors value and send to receiver") {
-  int sensordatalen = 8;
-   float TempArray[sensordatalen] ={2,3, 3, 5, 4, 10, 11, 12};
-   float ChargerateArray[sensordatalen] ={2,3, 3, 5, 4, 10, 11, 12};
-
-   REQUIRE(Main(TempArray,ChargerateArray,sensordatalen,3,10,5,11) == 1);
+  
    
 }
