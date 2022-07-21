2b44765c4f786cc5929a71556bcc71596bf1a858
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-07-21 23:55:56 +0530

    Add files via upload

---
+++ Receiver/helper.hpp
@@ -0,0 +1,99 @@
+
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+#define TOTAL_COUNT 5
+
+struct sensorData
+{
+    vector<int> chargeRateList;
+    vector<int> temperatureValueList;
+};
+
+struct sensorStatistics
+{
+    float chargeRateAvg;
+    float tempAvg;
+    int minChargeRate;
+    int maxChargeRate;
+    int minTemp;
+    int maxTemp;
+};
+
+class sensorInterface
+{
+public:
+     virtual sensorData getSensorDataFromConsole()=0;
+};
+
+class sensorReader: public sensorInterface
+{
+   public:
+       sensorData sensorInterface()
+       {
+            sensorData data;
+    	    string delimiter;
+			std::string sensorInput;
+            getline(cin, sensorData);
+            for(int sensorValue = 0; sensorValue < 5; sensorValue++)
+            {
+	        cin >> sensorData.chargeRateList[sensorValue] >> sensorValue >> sensorData.temperatureValueList[temperatureValueList];
+            }
+            return data;
+       }
+};
+
+class mockSensorReader: public sensorInterface
+{
+  public:
+       int counter=0;
+       sensorData mockData;
+
+       sensorData getSensorDataFromConsole()
+       {
+            sensorData dataContainer;
+            for(int sensorValue= 0; sensorValue< TOTAL_COUNT; sensorValue++)
+            {
+                
+                   dataContainer.chargeRateList.push_back(mockData.chargeRateList[sensorValue]);
+				   dataContainer.temperatureValueList.push_back(mockData.temperatureValueList[sensorValue]);
+                 
+            }
+            return dataContainer;
+      }
+};
+
+class displaySensorData
+{
+public:
+     virtual void displayDataonConsole(sensorValueStatistic sensorValue)=0;
+};
+
+class sensorValuePrinter: public displaySensorData
+{
+   public:
+       void displayDataonConsole(sensorStatistics stats)
+       {
+		   cout<<"Average: Charge sensor value="<<to_string(stats.chargeRateAvg)<<"\n";
+    	   cout<<"Average: Temperature sensor value="<<to_string(stats.tempavg);
+    	   cout<<"Charge sensor minimum value="<<to_string(stats.minChargeRate);
+    	   cout<<"Temperature sensor minimum value="<<to_string(stats.minTemp);
+    	   cout<<"Charge sensor maximum value="<<to_string(stats.maxChargeRate);
+    	   cout<<"Temperature sensor maximum value="<<to_string(stats.maxTemp);
+      }
+};
+
+class mockPrinter: public displaySensorData
+{
+   public:
+       int displayCounter=0;
+      
+       void displayDataonConsole(sensorValueStatistic /*sensorValue*/)
+       {
+	   for(int counter=0;counter<6;counter++)
+	   {
+		   displayCounter++;
+		   cout<<"Sensor Statistics";
+       }
+};
