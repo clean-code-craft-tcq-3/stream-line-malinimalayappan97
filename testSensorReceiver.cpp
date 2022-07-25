8c187cab78d91caab8be55a63372760fb99c9e31
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-07-25 23:52:47 +0530

    Update testSensorReceiver.cpp

--- testSensorReceiver.cpp
+++ testSensorReceiver.cpp
@@ -20,14 +20,14 @@ TEST_CASE("Test the computation of minimum value in the sensor data list where t
 {
         vector<int> sensorValueList={20};
         int min=getMinValue(sensorValueList);
-        REQUIRE(minValue==20);
+        REQUIRE(min==20);
 }
 
 TEST_CASE("Test the computation of maximum value in the sensor data list")
 { 
         vector<int> sensorValueList={10,30,20,40,45,25,70,30,70,35};
         int max=getMaxValue(sensorValueList);
-        REQUIRE(maxValue==70);
+        REQUIRE(max==70);
 }
 
 TEST_CASE("Test functionality to read Sensor data from Console using mock functionality")
@@ -47,8 +47,8 @@ TEST_CASE("Test the Sensor Value Statistics obtained")
         sensorData mockSensorValue;
         mockSensorValue.chargeRateList={100,30,20,20,45,25,70,30,90,35};
         mockSensorValue.temperatureValueList={60,80,20,30,45,10,70,40,90,50};
-        sensorReader dataReader;
-        dataReader.mockSensorData=mockSensorValue;
+        mockSensorReader dataReader;
+        dataReader.mockData=mockSensorValue;
         mockPrinter dataPrinterObject;
         sensorStatistics sensorDataStats=getSensorValueStatis(dataReader,dataPrinterObject); 
 		REQUIRE(sensorDataStats.chargeRateAvg==50.000000);  
