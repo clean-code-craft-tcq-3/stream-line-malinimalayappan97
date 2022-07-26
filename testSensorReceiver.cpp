8a24a0c003af1d82ea3580e8acd80bef2e9a4dab
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-07-26 07:44:59 +0530

    Update testSensorReceiver.cpp

--- testSensorReceiver.cpp
+++ testSensorReceiver.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Test the computation of Simple Moving Average")
 TEST_CASE("Test the computation of minimum value in the sensor data list")
 {
         vector<int> sensorValueList={10,30,20,30,45,25,70,30,70,35};
-		int min=getMinValue(sensorValueList);
+	int min=getMinValue(sensorValueList);
         REQUIRE(min==10);
 }
 
@@ -45,16 +45,16 @@ TEST_CASE("Test functionality to read Sensor data from Console using mock functi
 TEST_CASE("Test the Sensor Value Statistics obtained")
 {
         sensorData mockSensorValue;
-        mockSensorValue.chargeRateList={100,30,20,20,45,25,70,30,90,35};
-        mockSensorValue.temperatureValueList={60,80,20,30,45,10,70,40,90,50};
+        mockSensorValue.chargeRateList={100,30,20,20,45};
+        mockSensorValue.temperatureValueList={60,80,20,30,45};
         mockSensorReader dataReader;
         dataReader.mockData=mockSensorValue;
         mockPrinter dataPrinterObject;
         sensorStatistics sensorDataStats=getSensorValueStatis(dataReader,dataPrinterObject); 
-		REQUIRE(sensorDataStats.chargeRateAvg==43.000000);  
-  		REQUIRE(sensorDataStats.tempAvg==47.000000);
-		REQUIRE(sensorDataStats.minChargeRate==20);
-    	REQUIRE(sensorDataStats.minTemp==10);
+	REQUIRE(sensorDataStats.chargeRateAvg==43.000000);  
+  	REQUIRE(sensorDataStats.tempAvg==47.000000);
+	REQUIRE(sensorDataStats.minChargeRate==20);
+    	REQUIRE(sensorDataStats.minTemp==20);
     	REQUIRE(sensorDataStats.maxChargeRate==100);
-    	REQUIRE(sensorDataStats.maxTemp==90);
+    	REQUIRE(sensorDataStats.maxTemp==80);
 }
